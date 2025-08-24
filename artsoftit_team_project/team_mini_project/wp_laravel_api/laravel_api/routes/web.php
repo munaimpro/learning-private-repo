44b4732843d30f35c49a65cbaeb129be1cf2d3b0
Munaim Khan <khanmail2599@gmail.com>
2025-08-24 19:33:34 +0600

    route and view create for showing user list

--- artsoftit_team_project/team_mini_project/wp_laravel_api/laravel_api/routes/web.php
+++ artsoftit_team_project/team_mini_project/wp_laravel_api/laravel_api/routes/web.php
@@ -3,13 +3,12 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 
 // Route for get API
 Route::get('/users/get', ['getUsers', UserController::class]);
 
 // Route for post API
 Route::post('users/post', ['insertUser', UserController::class]);
+
+// Route for view user list
+Route::view('/', 'home');
