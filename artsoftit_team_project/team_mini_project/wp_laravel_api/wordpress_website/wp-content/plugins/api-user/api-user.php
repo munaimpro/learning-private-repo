ca3b305d0cfe2f1c5366686be7bd4a00ee7352b7
Munaim Khan <khanmail2599@gmail.com>
2025-08-27 19:53:13 +0600

    plugin basic setup

---
+++ artsoftit_team_project/team_mini_project/wp_laravel_api/wordpress_website/wp-content/plugins/api-user/api-user.php
@@ -0,0 +1,14 @@
+<?php
+/** 
+ * Plugin name: API User
+ * Plugin URI: 
+ * Description: This is a plugin for fetch, update, insert and delete users data from external Laravel API
+ * Version: 1.0.0
+ * Requires at least: 5.8
+ * Requires PHP: 7.2
+ * License: GPLv2 or later
+ * Text Domain: apiuser
+*/
+
+
+?>
