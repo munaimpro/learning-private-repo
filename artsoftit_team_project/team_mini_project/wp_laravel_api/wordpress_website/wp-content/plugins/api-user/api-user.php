d9f89d731f0ad78f040d2d2822b3df730f4f8108
Munaim Khan <khanmail2599@gmail.com>
2025-08-28 14:05:53 +0600

    render admin page on plugin activation

--- artsoftit_team_project/team_mini_project/wp_laravel_api/wordpress_website/wp-content/plugins/api-user/api-user.php
+++ artsoftit_team_project/team_mini_project/wp_laravel_api/wordpress_website/wp-content/plugins/api-user/api-user.php
@@ -11,4 +11,71 @@
 */
 
 
+// Function to add api user custom admin page on plugin activate
+add_action('admin_menu', 'register_admin_api_user_page');
+function register_admin_api_user_page () {
+    add_menu_page(
+        page_title: 'Laravel API User',
+        menu_title: 'API User',
+        capability: 'manage_options',
+        menu_slug: 'api-users',
+        callback: 'render_admin_page',
+        icon_url: 'dashicons-admin-users',
+        position: 6
+    );
+}
+
+
+// Function to render the external-users admin page
+function render_admin_page () {
+    ?>
+        <div class="wrap">
+            <h1>API Users</h1>
+            <!-- <form action="" method="POST">
+                <input type="submit" name="fetch_external_users" class="button button-primary" value="Fetch & Save Users">
+            </form> -->
+            
+            <p>Use the shortcode <strong>[laravel-api-users]</strong> to load these data on the frontend</p>
+
+            <table class="wp-list-table widefat fixed striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Profile</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+    
+                <tbody>
+                    <?php
+                        // if ($results) {
+                            // foreach ($results as $user) {
+                                echo '<tr>';
+                                echo '<td>' . esc_html('1') . '</td>';
+                                echo '<td>' . esc_html('<img src="">') . '</td>';
+                                echo '<td>' . esc_html('Samiul Alam') . '</td>';
+                                echo '<td>' . esc_html('samiul@gmail.com') . '</td>';
+                                echo '<td>' . esc_html('01823456789') . '</td>';
+                                echo '<td>
+                                        <a href="?page=external-users&action=edit&id=' . 1 . '" class="button">Edit</a>
+                                        <a href="?page=external-users&action=delete&id=' . 1 . '" class="button delete-user">Delete</a>
+                                    </td>';
+                                echo '</tr>';
+                            // }
+                        // } else {
+                        //     echo "<tr>
+                        //             <td colspan='5'>No users found.</td>
+                        //         </tr>";
+                        // }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    <?php
+}
+
+
 ?>
